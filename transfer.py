d913ae54f2ba5a19f702981d9ada9cbc2318ff45
Angela Hao Jiang <ahjiang@ops.orca.pdl.cmu.edu>
2017-03-30 19:45:41 -0400

    Runs optimization with new softmax layer Can not generalize to other layers yet

--- transfer.py
+++ transfer.py
@@ -1,38 +1,76 @@
 
 # Functions and classes for loading and using the Inception model.
-import inception
-from inception import transfer_values_cache
 import cifar10
-from cifar10 import num_classes
+import inception
+import numpy as np
 import os
+import prettytensor as pt
 import setup
+import tensorflow as tf
+import time
 
-def calculate_transfer_values():
-    file_path_cache_train = os.path.join(cifar10.data_path, 'inception_cifar10_train.pkl')
-    file_path_cache_test = os.path.join(cifar10.data_path, 'inception_cifar10_test.pkl')
+from datetime import timedelta
+from inception import transfer_values_cache
+from cifar10 import num_classes
 
-    print("Processing Inception transfer-values for training-images ...")
+train_batch_size = 64		# lower and add iterations if running out of RAM
 
-    # Scale images because Inception needs pixels to be between 0 and 255,
-    # while the CIFAR-10 functions return pixels between 0.0 and 1.0
-    images_scaled = images_train * 255.0
+def random_batch():
+    # Number of images (transfer-values) in the training-set.
+    num_images = len(transfer_values_train)
 
-    # If transfer-values have already been calculated then reload them,
-    # otherwise calculate them and save them to a cache-file.
-    transfer_values_train = transfer_values_cache(cache_path=file_path_cache_train,
-                                                  images=images_scaled,
-                                                  model=model)
-    print("Processing Inception transfer-values for test-images ...")
+    # Create a random index.
+    idx = np.random.choice(num_images,
+                           size=train_batch_size,
+                           replace=False)
 
-    # Scale images because Inception needs pixels to be between 0 and 255,
-    # while the CIFAR-10 functions return pixels between 0.0 and 1.0
-    images_scaled = images_test * 255.0
+    # Use the random index to select random x and y-values.
+    # We use the transfer-values instead of images as x-values.
+    x_batch = transfer_values_train[idx]
+    y_batch = labels_train[idx]
 
-    # If transfer-values have already been calculated then reload them,
-    # otherwise calculate them and save them to a cache-file.
-    transfer_values_test = transfer_values_cache(cache_path=file_path_cache_test,
-                                                 images=images_scaled,
-                                                 model=model)
+    return x_batch, y_batch
+
+def optimize(num_iterations):
+    # Start-time used for printing time-usage below.
+    start_time = time.time()
+
+    for i in range(num_iterations):
+        # Get a batch of training examples.
+        # x_batch now holds a batch of images (transfer-values) and
+        # y_true_batch are the true labels for those images.
+        x_batch, y_true_batch = random_batch()
+
+        # Put the batch into a dict with the proper names
+        # for placeholder variables in the TensorFlow graph.
+        feed_dict_train = {x: x_batch,
+                           y_true: y_true_batch}
+
+        # Run the optimizer using this batch of training data.
+        # TensorFlow assigns the variables in feed_dict_train
+        # to the placeholder variables and then runs the optimizer.
+        # We also want to retrieve the global_step counter.
+        i_global, _ = session.run([global_step, optimizer],
+                                  feed_dict=feed_dict_train)
+
+        # Print status to screen every 100 iterations (and last).
+        if (i_global % 100 == 0) or (i == num_iterations - 1):
+            # Calculate the accuracy on the training-batch.
+            batch_acc = session.run(accuracy,
+                                    feed_dict=feed_dict_train)
+
+            # Print status.
+            msg = "Global Step: {0:>6}, Training Batch Accuracy: {1:>6.1%}"
+            print(msg.format(i_global, batch_acc))
+
+    # Ending time.
+    end_time = time.time()
+
+    # Difference between start and end-times.
+    time_dif = end_time - start_time
+
+    # Print the time-usage.
+    print("Time usage: " + str(timedelta(seconds=int(round(time_dif)))))
 
 setup.setup()   # Download ciphar and inception
 
@@ -44,7 +82,32 @@ class_names = cifar10.load_class_names()
 # Get inception model
 model = inception.Inception()
 
-calculate_transfer_values()
+# Calculate transfer values
+file_path_cache_train = os.path.join(cifar10.data_path, 'inception_cifar10_train.pkl')
+file_path_cache_test = os.path.join(cifar10.data_path, 'inception_cifar10_test.pkl')
+
+print("Processing Inception transfer-values for training-images ...")
+
+# Scale images because Inception needs pixels to be between 0 and 255,
+# while the CIFAR-10 functions return pixels between 0.0 and 1.0
+images_scaled = images_train * 255.0
+
+# If transfer-values have already been calculated then reload them,
+# otherwise calculate them and save them to a cache-file.
+transfer_values_train = transfer_values_cache(cache_path=file_path_cache_train,
+                                              images=images_scaled,
+                                              model=model)
+print("Processing Inception transfer-values for test-images ...")
+
+# Scale images because Inception needs pixels to be between 0 and 255,
+# while the CIFAR-10 functions return pixels between 0.0 and 1.0
+images_scaled = images_test * 255.0
+
+# If transfer-values have already been calculated then reload them,
+# otherwise calculate them and save them to a cache-file.
+transfer_values_test = transfer_values_cache(cache_path=file_path_cache_test,
+                                             images=images_scaled,
+                                             model=model)
 
 transfer_len = model.transfer_len
 # Placeholder for inputting transfer values from Inception to new model
@@ -63,11 +126,6 @@ with pt.defaults_scope(activation_fn=tf.nn.relu):
 global_step = tf.Variable(initial_value=0,
                           name='global_step', trainable=False)
 
-opt_vars = [v for v in tf.trainable_variables() if v.name.startswith("layer17/")]
-
-print("================== Opt Vars ==================")
-print(opt_vars)
-
 # http://stackoverflow.com/questions/38749120/fine-tuning-a-deep-neural-network-in-tensorflow
 optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss, global_step)
 
@@ -83,4 +141,4 @@ print(accuracy)
 session = tf.Session()
 session.run(tf.global_variables_initializer())
 
-
+optimize(num_iterations=10000)
